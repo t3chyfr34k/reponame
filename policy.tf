24c18ea8b1898862c8e2829b1bbd9aa3c43ff996
t3chyfr34k <djshyamdr15@gmail.com>
2025-07-14 15:03:33 +0530

    pust

--- policy.tf
+++ policy.tf
@@ -1,25 +1,6 @@
-resource "aws_iam_policy" "read_only_s3" {
-  name        = "ReadOnlyS3Policy"
-  path        = "/"
-  description = "Grants read-only access to S3 buckets"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:Get*",
-          "s3:List*"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "attach_s3_readonly" {
-  name       = "attach_readonly_policy"
+resource "aws_iam_policy_attachment" "attach_admin_policy" {
+  name       = "attach_admin_access"
   users      = [aws_iam_user.t3chy.name]
-  policy_arn = aws_iam_policy.read_only_s3.arn
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
