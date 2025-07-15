3c1a63099ba35d79e827b695db26121f266fc67a
t3chyfr34k <djshyamdr15@gmail.com>
2025-07-15 11:24:05 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy_attachment" "attach_admin_policy" {
   name       = "attach_admin_access"
-  users      = [aws_iam_user.t3chy.name]
+  users      = [aws_iam_user.t3chy.internal]
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
